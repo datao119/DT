0b372d39232b79d36bcdd5c75104a6c020ace6b0
datao <datao119@163.com>
2019-12-12 10:50:18 +0800

    mango_platform

---
+++ mango-platform/后端源码/9. 系统服务监控/mango-admin/src/main/java/com/louis/mango/admin/dao/SysLogMapper.java
@@ -0,0 +1,24 @@
+package com.louis.mango.admin.dao;
+
+import com.louis.mango.admin.model.SysLog;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SysLogMapper {
+    int deleteByPrimaryKey(Long id);
+
+    int insert(SysLog record);
+
+    int insertSelective(SysLog record);
+
+    SysLog selectByPrimaryKey(Long id);
+
+    int updateByPrimaryKeySelective(SysLog record);
+
+    int updateByPrimaryKey(SysLog record);
+    
+    List<SysLog> findPage();
+    
+    List<SysLog> findPageByUserName(@Param(value="userName") String userName);
+}
